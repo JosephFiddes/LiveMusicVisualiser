435126ff6aff39803012a3b7357ecc111d7c5ecb
Joseph Fiddes <fiddj13@live.com.au>
2026-01-21 19:51:36 +1100

    Got a basic filter working (currently displays nothing)

--- obs_effect_interface.lua
+++ obs_effect_interface.lua
@@ -1,57 +1,110 @@
 obs = obslua
 
-Effect_Interface = {
-    source_info = {},
-    implementation = {},
-    properties = {}
-}
+local Effect_Interface = {}
+Effect_Interface.__index = Effect_Interface
 
-function Effect_Interface:new(id, name, flags, create, destroy)
+local function bind(obj, fn)
+    return function(...)
+        return fn(obj, ...)
+    end
+end
+
+function Effect_Interface.new(id, name, flags, create, destroy)
+    local self = setmetatable({}, Effect_Interface)
+
     self.source_info = {}
     self.source_info.id = id
     self.source_info.type = obs.OBS_SOURCE_TYPE_FILTER
     self.source_info.output_flags = flags
     self.source_info.get_name = function() return name end
 
+    self.implementation = {}
     self.implementation.create = create
     self.implementation.destroy = destroy
 
-    self.source_info.create = function(settings, source) self.__create(setting, source); end
-    self.source_info.destroy = function(data) self.__destroy(data); end
-    self.source_info.update = function(data, settings), self.__update(data, settings); end
-    self.source_info.video_render = function(data), self.__video_render(data); end
+    self.source_info.create = bind(self, Effect_Interface._create_impl)
+    self.source_info.destroy = bind(self, Effect_Interface._destroy_impl)
+    self.source_info.get_defaults = bind(self, Effect_Interface._get_defaults_impl)
+    self.source_info.get_properties = bind(self, Effect_Interface._get_properties_impl)
+    self.source_info.update = bind(self, Effect_Interface._update_impl)
+    self.source_info.video_render = bind(self, Effect_Interface._video_render_impl)
 
+    self.params = {}
 
+    return self
 end
 
-function Effect_Interface:__create(settings, source)
+function Effect_Interface:add_parameter(name, disp_name, 
+    on_create, get_default, get_properties, on_update, on_video_render)
+    local param = {
+        name = name,
+        disp_name = disp_name,
+        on_create = on_create,
+        get_default = get_default,
+        get_properties = get_properties,
+        on_update = on_update,
+        on_video_render = on_video_render
+    }
 
+    table.insert(self.params, param)
+end
+
+function Effect_Interface._create_impl(self, settings, source)
     data = self.implementation.create(settings, source)
 
-    data.params = -- some function to get all the params (iterates through a bunch of callbacks)
+    for _, param in ipairs(self.params) do
+        param.on_create(param.name, param.disp_name, data)
+    end
 
     self.source_info.update(data, settings)
+
+    return data
 end
 
-function Effect_Interface:__destroy(data)
+function Effect_Interface._destroy_impl(self, data)
     self.implementation.destroy(data)
 end
 
-function Effect_Interface:__update(data, settings)
-    data.thingo -- iterate through, getting data
+function Effect_Interface._get_defaults_impl(self, settings)
+    for _, param in ipairs(self.params) do
+        param.get_default(param.name, param.disp_name, settings)
+    end
+end
 
+function Effect_Interface._get_properties_impl(self, data)
+    local props = obs.obs_properties_create()
+
+    for _, param in ipairs(self.params) do
+        param.get_properties(param.name, param.disp_name, props, data)
+    end
+
+    return props
+end
+
+function Effect_Interface._update_impl(self, data, settings)
+    for _, param in ipairs(self.params) do
+        param.on_update(param.name, param.disp_name, data, settings)
+    end
     data.settings = settings
 end
 
-function Effect_Interface:__video_render(data)
+function Effect_Interface._video_render_impl(self, data)
     
 	local parent = obs.obs_filter_get_parent(data.source)
 	data.width = obs.obs_source_get_base_width(parent)
 	data.height = obs.obs_source_get_base_height(parent)
 
 	obs.obs_source_process_filter_begin(data.source, obs.GS_RGBA, obs.OBS_NO_DIRECT_RENDERING)
-
-    -- do some iterative stuff here
+    
+    for _, param in ipairs(self.params) do
+        param.on_video_render(param.name, param.disp_name, data)
+    end
     
 	obs.obs_source_process_filter_end(data.source, data.effect, data.width, data.height)
 end
+
+function Effect_Interface:register_source()
+    obs.obs_register_source(self.source_info)
+end
+
+return Effect_Interface
--- visualize_sound.lua
+++ visualize_sound.lua
@@ -1,13 +1,59 @@
+obs = obslua
+
+local Effect_Interface = require("obs_effect_interface")
+
 function script_description()
 	return [[<center><h2>Sound Visualiser</h2></center>
 		<p>Visualise sound.</p>]]
 end
 
 function script_load(settings)
-    sound_visualiser_interface = Effect_Interface:new("sound-vis", "Sound Visualiser", 
-        obs.OBS_SOURCE_VIDEO, function() return 0 end, function() return 0 end)
+    local sound_visualiser_interface = Effect_Interface.new("sound-vis", "Sound Visualiser", 
+        obs.OBS_SOURCE_VIDEO, 
+		-- create()
+		function(settings, source) 
+			-- Initializes the custom data table
+			local data = {}
+			data.source = source
+			data.width = 1
+			data.height = 1
+			
+			data.params = {}
 
-    
+			return data
+		end, 
+		-- destroy()
+		function(data) 
+			if data.effect == nil then return end
 
-	sound_visualiser_interface.register_source()
+			obs.obs_enter_graphics()
+			obs.gs_effect_destroy(data.effect)
+			data.effect = nil
+			obs.obs_leave_graphics()
+		end)
+
+	sound_visualiser_interface:add_parameter("gamma", "Gamma encoding exponent", 
+		-- on_create() (called in source_info.create)
+		function(name, disp_name, data) 
+			-- data.params.gamma = obs.gs_effect_get_param_by_name(data.effect, name)
+			data.params.gamma = "it's gammaing time"
+		end,
+		-- get_default() (called in source_info.get_defaults)
+		function(name, disp_name, settings) 
+  			obs.obs_data_set_default_double(settings, name, 1.0)
+		end,
+		-- get_properties() (called in source_info.get_properties)
+		function(name, disp_name, props, data) 
+  			obs.obs_properties_add_float_slider(props, name, disp_name, 1.0, 2.2, 0.2)
+		end,
+		-- on_update() (called in source_info.update)
+		function(name, disp_name, data, settings) 
+			data.gamma = obs.obs_data_get_double(settings, name)
+		end,
+		-- on_video_render() (called in source_info.video_render)
+		function(name, disp_name, data) 
+			-- obs.gs_effect_set_float(data.params.gamma, data.gamma)
+		end)
+
+	sound_visualiser_interface:register_source()
 end
