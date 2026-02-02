24e4416a3cf602d81cdf56df0b2482e5441516e5
Joseph Fiddes <fiddj13@live.com.au>
2026-02-02 17:31:38 +1100

    Added basic info

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -24,11 +24,11 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 bool obs_module_load(void)
 {
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	obs_log(LOG_INFO, "JDF's plugin loaded successfully (version %s)", PLUGIN_VERSION);
 	return true;
 }
 
 void obs_module_unload(void)
 {
-	obs_log(LOG_INFO, "plugin unloaded");
+	obs_log(LOG_INFO, "JDF's plugin unloaded");
 }
